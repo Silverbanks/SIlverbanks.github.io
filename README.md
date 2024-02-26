dcfbfb8543713232695d198b2ed2d60720220821
Silverbanks <118997033+Silverbanks@users.noreply.github.com>
2024-02-26 09:55:42 +0100

    set homepage for github pages

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# SIlverbanks.github.io
+#SIlverbanks.github.io
+My Portfolio Website
+
+silverbanks.github.io
+
+
+
+Hi there, I’m Jane Doe 👋
+I’m a web developer and designer who loves to create beautiful and functional websites. This is my portfolio website where I showcase some of my projects and skills.
+
+Technologies
+I built this website using the following technologies:
+
+HTML
+CSS
+JavaScript
+Bootstrap
+jQuery
+Font Awesome
+Installation and Usage
+To run this website locally, you need to have Node.js and npm installed on your machine.
+
+Clone this repository: git clone https://github.com/Silverbanks/silverbanks.github.io.git
+Navigate to the project folder: cd portfolio
+Install the required dependencies: npm install
+Start the development server: npm start
+Open your browser and go to http://localhost:3000
+Alternatively, you can view the live demo of this website here.
+
+Features
+This website has the following features:
+
+A responsive and elegant design that adapts to different screen sizes and devices
+A navigation bar that allows you to access different sections of the website
+A home section that displays a welcome message and a brief introduction about me
+A portfolio section that showcases some of my projects with images, titles, descriptions, and links
+A skills section that lists some of the technologies, tools, and frameworks that I use
+A contact section that contains a form where you can send me a message or an email
+A footer that contains some social media icons and links to my other online platforms
+
+
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+Code of Conduct
+Please note that this project is released with a [Contributor Code of Conduct]. By participating in this project you agree to abide by its terms.
+
+Contributing
+I welcome any contributions, suggestions, or feedback. If you want to contribute to this project, please follow these step:
+
+Fork this repository
+Create a new branch: git checkout -b <branch-name>
+Make your changes and commit them: git commit -m '<commit-message>'
+Push to the original branch: git push origin <porfolio-project>/<silverbanks.github.io>
+Create a pull request
+Please refer to the [CONTRIBUTING] file for more information.
+
+Badges
+![GitHub stars] ![GitHub forks] ![GitHub issues] ![GitHub pull requests] ![GitHub license]
+
+Statistics
+![GitHub stats] ![GitHub streak stats] ![GitHub language stats]
+
+Links
+You can find me on:
+
+[Twitter]
+[LinkedIn]
+[Medium]
+[CodePen]
+You can also visit my [personal website] or check out my [resume].
+
+Contact
+If you have any questions, comments, or suggestions, feel free to contact me at silver127@gmx.us I’d love to hear from you! 😊
---
+++ script.js
@@ -0,0 +1,174 @@
+var audio = new Audio('assets/sentmessage.mp3');
+var contactString = "<div class='social'> <a target='_blank' href='tel:+2348039577712'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>8039577712</label></div> </a> <a href='mailto:silverchiahemba@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/Silverbanks'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/2348039577712'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> </a> <a target='_blank' href='https://t.me/Silverbanks'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/silverbanks'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/silver-chiahemba-992a91208' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Silver Resume.pdf</label></div><a href='assets/Silver Chiahemba&#39;s Resume.pdf' download='Silver_Chiahemba_Resume.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
+var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/place/6%C2%B013'22.1%22N+7%C2%B003'30.7%22E/@6.2227912,7.0559648,17z/data=!3m1!4b1!4m4!3m3!8m2!3d6.2227912!4d7.0585397?entry=ttu' class='map'></iframe></div><label class='add'><address>34 'Sen. Uche Ekwunife'<br>Crescent<br>Iyiagu Estate, AWKA 420107, ANAMBRA STATE, NG</address>";
+function startFunction() {
+    setLastSeen();
+    waitAndResponce("intro");
+}
+
+function setLastSeen() {
+    var date = new Date();
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "last seen today at " + date.getHours() + ":" + date.getMinutes()
+}
+
+
+function closeFullDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+function openFullScreenDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+
+function isEnter(event) {
+    if (event.keyCode == 13) {
+        sendMsg();
+    }
+}
+
+function sendMsg() {
+    var input = document.getElementById("inputMSG");
+    var ti = input.value;
+    if (input.value == "") {
+        return;
+    }
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "sent");
+    greendiv.setAttribute("class", "green");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = input.value;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    setTimeout(function () { waitAndResponce(ti) }, 1500);
+    input.value = "";
+    playSound();
+}
+
+function waitAndResponce(inputText) {
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "typing...";
+    switch (inputText.toLowerCase().trim()) {
+        case "intro":
+            setTimeout(() => {
+                sendTextMessage("Hey there👋🏻,<br><br>I am<span class='bold'><a class='alink'>Silver</a>.</span><br><br>I am a Software Engineering student at <span class='bold'>ALX-Hoberton School 👨🏻‍💻📚</span><br><br>I am also an undergrad student of the National Open University of Nigeria. I am eager to hear about potential career opportunities, so I would be happy to engage about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+            }, 2000);
+            break;
+        case "help":
+            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'comment'</span> - to leave comments<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
+            break;
+        case "resume":
+            sendTextMessage(resumeString);
+            break;
+        case "skills":
+            sendTextMessage("<span class='sk'>I am currently pursuing a B.Sc in Information Technology.<br><br>I can develop software with the following languages :<br><span class='bold'><br>C<br>PHP<br>Python<br>CSS<br>HTML<br>JavaScript</span><br><br>I have experience with following frameworks :<span class='bold'>Bootstrap<br>Flask<br>NumPy<br>jQuery<br></span><br>My skills include:<span class='bold'>programming and coding<br>Debugging<br>Database Administration<br>Software Development<br>APIs<br></span><br>Favorite IDE:VSCode</span>");
+            break;
+
+        case "education":
+            sendTextMessage("I am currently pursuing a B.Sc in Information Technology from National Open University of Nigeria<br>Graduating Year : July 2024<br><br>I am about to go into specialization at ALX Hoberton School, where I'm studying Software Engineering<br>Graduation will be June 2024<br><br>I studied CCNA Routing and Switching in 2015");
+            break;
+
+        case "address":
+            sendTextMessage(addressString);
+            break;
+        case "clear":
+            clearChat();
+            break;
+        case "about":
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https:\/\/instagram.com/silverbankz/'><span class='bold'>Silver Chiahemba</a> with ❤️</span>");
+            break;
+        case "contact":
+            sendTextMessage(contactString);
+            break;
+        case "projects":
+            sendTextMessage("Wanna checkout my projects? Here's my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/Silverbanks'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            break;
+        case "new":
+            sendTextMessage(addressString);
+            break;
+        default:
+            setTimeout(() => {
+                sendTextMessage("Hey I couldn't catch you...😢<br>Send 'help' for guide.");
+            }, 2000);
+            break;
+    }
+
+
+
+}
+
+function clearChat() {
+    document.getElementById("listUL").innerHTML = "";
+    waitAndResponce('intro');
+}
+
+
+
+function sendTextMessage(textToSend) {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.setAttribute("id", "sentlabel");
+    dateLabel.id = "sentlabel";
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    greendiv.innerHTML = textToSend;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+
+function sendResponse() {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ";
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+function playSound() {
+    audio.play();
+}
